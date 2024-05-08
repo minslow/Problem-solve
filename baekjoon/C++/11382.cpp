0265f098f86d32a769d8ec3d06d8b979d3df7094
minslow <71661658+minslow@users.noreply.github.com>
2024-05-08 11:05:31 +0900

    Add baekjoon quiz

---
+++ baekjoon/C++/11382.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+//출력 문제 11번 - 입력값의 크기를 고려하지 못하고 있었음
+
+int main()
+{
+    long int n1;
+    long int n2;
+    long int n3;
+
+    cin >> n1 >> n2 >> n3;
+    cout << n1 + n2 + n3 << endl;
+}
